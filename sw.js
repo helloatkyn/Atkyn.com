4a22ee25a42ff6ea83f0c82600ab8993e7730009
atkynsystem <atkyn.system@hotmail.com>
2026-07-21 13:28:23 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'atkyn-v1';
+const STATIC = ['/', '/index.html', '/Atkyn.png', '/icon-192.png', '/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(STATIC)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // Cache-first for static assets, network-first for everything else
+  const url = new URL(e.request.url);
+  const isStatic = e.request.destination === 'image' ||
+                   url.pathname.endsWith('.png') ||
+                   url.pathname.endsWith('.html') ||
+                   url.pathname === '/';
+
+  if (isStatic) {
+    e.respondWith(
+      caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }))
+    );
+  } else {
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match(e.request))
+    );
+  }
+});
